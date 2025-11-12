0cbe1f795eccc4fa210615765aad3ecdfde747b2
Kabir <adkabir@julius-berger.com>
2025-11-12 10:30:18 +0100

    My Tavily tool using both my  custom tool and the tavily 'langchain_tavily' tool

--- main_search_agent.py
+++ main_search_agent.py
@@ -1,78 +1,65 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
 
 from dotenv import load_dotenv
 load_dotenv()
 import os
-
 open_ai_key = os.environ.get("OPENAI_API_KEY")
 
-
-
-########################################################################
-
-
-## BAckup main
+from langchain.agents import create_agent
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain.tools import tool
 
 
 
+#############  dont need the below part because we are going to use the langchai_tavily library instead of our custom tool ####
+# from tavily import TavilyClient
+# tavily = TavilyClient()
 
 
-
-# from langchain_core.prompts import PromptTemplate
-# from langchain_openai import ChatOpenAI
-# from langchain_ollama import ChatOllama
-
-# from dotenv import load_dotenv
-# load_dotenv()
-# import os
-
-# open_ai_key = os.environ.get("OPENAI_API_KEY")
-
-# def main():
-#     # print("Hello World!")]
-#     information = """Elon Reeve Musk[a] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, 
-#     Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
-
-#     Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he had obtained Canadian citizenship at birth through his Canadian-born mother. 
-#     He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. 
-#     In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, 
-#     which was acquired by eBay in 2002. That year, Musk also became an American citizen.
-
-#     In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and 
-#     commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a 
-#     leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the 
-#     organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing 
-#     significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the 
-#     tunneling company the Boring Company, which he founded in 2017.
-
-#     Musk is a supporter of global far-right figures, causes, and political parties. He was the largest donor in the 2024 U.S. presidential election, where he 
-#     supported Donald Trump. After Trump was inaugurated as president in early 2025, Musk served as Senior Advisor to the President and as the de facto head of DOGE. 
-#     After a public feud with Trump, Musk left the Trump administration and returned to his technology companies.
+# @tool
+# def search(query: str) -> str:
+#     """Tool that searches the internet.
+#     Args:
+#         query: The query to search for.
+#     Returns:
+#         The results of the search.
 #     """
-    
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
-#     summary_template = """given the information {information} about a person, please create: 
-#     1. a short summary about them
-#     2. 2 interesting facts about them based strictly on the given information
-#     """
-#     # print("OPENAI_API_KEY:", open_ai_key)  ### TEST!!
+#############  dont need the above part because we are going to use the langchai_tavily library instead of our custom tool ####
 
-#     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-#     # llm = ChatOpenAI(model="gpt-5-mini", temperature=0, api_key=open_ai_key) ## openai version use api key
-#     llm = ChatOllama(model="gpt-oss:20b", temperature=0) ## ollama version
 
-#     chain = summary_prompt_template | llm                    ###### this is the 'langchain expression language' sysntax (LCEL)
-#     response = chain.invoke(input={"information": information})
-#     print(response.content)
-  
+llm = ChatOpenAI(model="gpt-5", temperature=0.0)
+# llm = ChatOllama(model="gpt-oss:20b", temperature=0.0)
+tools = [TavilySearch()]
+agent = create_agent(llm, tools)
 
 
 
 
 
-# if __name__ == "__main__":
-#     main()
+
+def main():
+    # print("Hello World!")
+    result = agent.invoke({"messages": [HumanMessage(content="search for 3 linkedin job postings in the bay area for an ai engineer with experience using \
+                                                      langchain,  and list their details",)]})  
+    # result = agent.invoke({"messages": [HumanMessage(content=input("Enter your query: "))]})  ## AN example using the additional dynamic functionality of "input()"
+    print(result)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 
